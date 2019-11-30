2a34ea5385a31da8661c0c9d05fe4f7ee2419327
TJunkie <tripjunkie24@gmail.com>
2019-11-30 21:02:44 +0300

    m

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -5,6 +5,7 @@ import Input from "../../components/UI/Input/Input";
 import Select from "../../components/UI/Select/Select";
 import {createControl, validate, validateForm} from '../../form/FormFramework';
 import Auxiliary from "../../hoс/Auxiliary/Auxiliary";
+import axios from '../../axios/axios-quiz';
 
 function createOptionControl(number) {
     return createControl({
@@ -68,10 +69,21 @@ export default class QuizCreator extends Component {
         })
     };
 
-    createQuestionHandler = event => {
+    createQuestionHandler = async event => {
         event.preventDefault();
 
-        console.log(this.state.quiz);
+        try {
+           await axios.post('/quizes.json', this.state.quiz);
+
+           this.setState({
+               quiz: [],
+               isFormValid: false,
+               rightAnswerId: 1,
+               formControls: createFormControls()
+           })
+        } catch (e) {
+            console.log(e)
+        }
     };
 
     changeHandler = (value, controlName) => {
--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,27 +1,50 @@
 import React, {Component} from 'react';
 import classes from './QuizList.css';
 import {NavLink} from "react-router-dom";
-import axios from 'axios';
+import Loader from "../../components/UI/Loader/Loader";
+import axios from '../../axios/axios-quiz';
 
 export default class QuizList extends Component {
+
+    state = {
+        quizes: [],
+        loading: true
+    };
+
     renderQuizes () {
-        return [1, 2, 3].map((quiz, index) => {
+        return this.state.quizes.map(quiz => {
             return (
                 <li
-                  key={index}
+                  key={quiz.id}
                 >
-                    <NavLink to={'/quiz/' + quiz}>
-                        Тест {quiz}
+                    <NavLink to={'/quiz/' + quiz.id}>
+                        {quiz.name}
                     </NavLink>
                 </li>
             )
         })
     }
 
-    componentDidMount() {
-        axios.get('https://react-quiz-52a9e.firebaseio.com/quiz.json').then(responce => {
-            console.log(responce);
-        })
+    async componentDidMount() {
+        try {
+            const response = await axios.get('/quizes.json');
+
+            const quizes = [];
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест №${index + 1}`
+                })
+            });
+
+            this.setState({
+                quizes, loading: false
+            })
+
+        } catch (e) {
+            console.log(e);
+        }
+
     }
 
     render() {
@@ -29,10 +52,13 @@ export default class QuizList extends Component {
             <div className={classes.QuizList}>
                 <div>
                     <h1>Список тестов</h1>
-
-                    <ul>
-                        {this.renderQuizes()}
-                    </ul>
+                    {
+                        this.state.loading
+                        ? <Loader/>
+                        : <ul>
+                            {this.renderQuizes()}
+                          </ul>
+                    }
                 </div>
             </div>
         );
