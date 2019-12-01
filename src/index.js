c26dc6f9def5366050dee08724e1bfad6a38b938
TJunkie <tripjunkie24@gmail.com>
2019-12-01 14:54:05 +0300

    m

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
 const app = (
    <BrowserRouter>
        <App/>
    </BrowserRouter>
 );
+
+const store = createStore();
+
 ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
